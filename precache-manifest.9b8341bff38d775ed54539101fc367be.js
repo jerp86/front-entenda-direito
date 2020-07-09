aa78c6d9db2165916de7c64e1176b60849a6d314
Jose Eduardo <jerp4@hotmail.com>
2020-07-09 10:29:52 -0300

    Updates

---
+++ precache-manifest.9b8341bff38d775ed54539101fc367be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3bc8f116ac235794917234c23e3f29",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e14fd7f38f5217ace4ff",
+    "url": "./static/js/2.54da4ff3.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.54da4ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600bc64c739dbbf82a1b",
+    "url": "./static/js/main.b13e0720.chunk.js"
+  },
+  {
+    "revision": "11ec2811ffb630de5d80",
+    "url": "./static/js/runtime-main.82ecfc9f.js"
+  },
+  {
+    "revision": "a49dcf9c8ccd3029da029bfc746c5e16",
+    "url": "./static/media/background.a49dcf9c.jpg"
+  },
+  {
+    "revision": "6c919960cea042ff6843b9dddc17aced",
+    "url": "./static/media/forgot-password.6c919960.jpg"
+  },
+  {
+    "revision": "c65fd4bfdcabdce19a2581f7543b3101",
+    "url": "./static/media/logo.c65fd4bf.svg"
+  },
+  {
+    "revision": "526a333a9a04b472ff8330fa9bac37cc",
+    "url": "./static/media/sign-up.526a333a.jpg"
+  }
+]);
